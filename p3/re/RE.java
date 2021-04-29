1b94c41e61e9fe22238b3d1e43a5c6fe98aed692
jtheck314 <55598884+jtheck314@users.noreply.github.com>
2021-04-29 16:08:39 -0600

    Add files via upload

--- p3/re/RE.java
+++ p3/re/RE.java
@@ -1,20 +1,98 @@
 package re;
 
+import java.util.Set;
+
+import fa.State;
 import fa.nfa.NFA;
+import fa.nfa.NFAState;
 
 public class RE implements REInterface {
 
 	private String regex;
+	private int stateCount;
+	private String currState;
+	private char currSymb;
 	
 	public RE(String input) {
 		this.regex = input;
+		this.stateCount = 0;
+		this.currState = "0";
+		this.currSymb = 'e';
+
 	}
 	
 	public NFA getNFA() {
 		
-	}
+		return regex();
 
-	public NFA parse () {...}
+	}
+	
+	private NFA nfaSequence(NFA from, NFA to) {
+		NFA n = new NFA();
+		
+		n = nfaUnion(n,from);
+		
+		n = nfaUnion(n,to);
+		
+		n.addStartState(from.getStartState().getName());
+		
+		for(State s : from.getFinalStates()) {
+			n.addTransition(s.getName(), 'e', to.getStartState().getName());
+		}
+		
+		for(State s : to.getFinalStates()) {
+			n.addFinalState(s.getName());
+		}
+		
+		return n;
+	}
+	
+	private NFA nfaChoice(NFA nfa1, NFA nfa2) {
+		NFA n = new NFA();
+		incrementState();
+		n.addStartState(currState);
+		
+		n = nfaUnion(n,nfa1);
+		
+		n = nfaUnion(n,nfa2);
+		
+		n.addTransition(n.getStartState().getName(), 'e', nfa1.getStartState().getName());
+		n.addTransition(n.getStartState().getName(), 'e', nfa2.getStartState().getName());
+				
+		for(State s : nfa1.getFinalStates()) {
+			n.addFinalState(s.getName());
+		}
+		
+		for(State s : nfa2.getFinalStates()) {
+			n.addFinalState(s.getName());
+		}
+		
+		return n;
+	}
+	
+	private NFA nfaUnion(NFA n, NFA m) {
+		for(State s : m.getStates()) {
+			n.addState(s.getName());
+		}
+		n.addAbc(m.getABC());
+		for(State s : m.getStates()) {
+			Set<Character> set = m.getABC();
+			set.add('e');
+			for(char c : set) {
+				Set<NFAState> toState = m.getToState((NFAState) s, c);
+				for(State f : toState) {
+					n.addTransition(s.getName(), c, f.getName());
+				}
+			}
+		}
+		return n;
+	}
+	
+	private void incrementState() {
+		this.stateCount++;
+		this.currState = String.valueOf(this.stateCount);
+		
+	}
 	
 	/* Recursive descent parsing internals. */
 	
@@ -25,8 +103,8 @@ public class RE implements REInterface {
 	private void eat(char c) {
 		if (peek() == c)
 			this.regex = this.regex.substring(1) ;
-    else
-    	throw new RuntimeException("Expected: " + c + "; got: " + peek()) ;
+		else
+			throw new RuntimeException("Expected: " + c + "; got: " + peek()) ;
 	}
   
 	private char next() {
@@ -39,6 +117,8 @@ public class RE implements REInterface {
 		return this.regex.length() > 0 ;
 	}
 	
+	
+	
 	/* Regular expression term types. */
 	
 	private NFA regex() {
@@ -46,18 +126,44 @@ public class RE implements REInterface {
 		
 		if (more() && peek() == '|') {
 		      eat ('|') ;
-		      NFA regex = regex() ;
-		      return new Choice(term,regex) ;
+		      NFA n = regex() ;
+		      return nfaChoice(t,n) ;
 	    } else {
 		     return t ;
 	    }
 	}
 	
 	private NFA term() {
-		//Test to see if vscode is setup correctly 
+		NFA factor = factor();
+
+	    while (more() && peek() != ')' && peek() != '|') {
+	    	NFA nextFactor = factor() ;
+	    	factor = nfaSequence(factor,nextFactor) ;
+	    }
+
+	    return factor ;
 	}
 	
-	private NFA factor() {...}
+	private NFA factor() {
+	    NFA base = base();
+	    
+	    if (more() && peek() == '*') {
+	    	eat('*') ;
+	    	for(State s : base.getFinalStates()) {
+		    	base.addTransition(s.getName(), 'e', base.getStartState().getName());
+	    	}
+	    	base.addFinalState(base.getStartState().getName());
+	    }
+	    else {
+	    	String fromState = currState;
+	    	incrementState();
+	    	base.addState(currState);
+	    	base.addTransition(fromState, currSymb, currState);
+	    	base.addFinalState(currState);
+	    }
+
+	    return base ;
+	}
 	
 	private NFA base() {
 		switch (peek()) {
@@ -67,13 +173,17 @@ public class RE implements REInterface {
 	    		eat(')') ;
 	    		return r ;
 
-	    	case 'e':
-	    		eat ('\\') ;
-	    		char esc = next() ;
-	    		return new Primitive(esc) ;
 
 	    	default:
-	    		return next() ;
+	    		incrementState();
+	    		NFA n = new NFA();
+	    		this.currSymb = next();
+	    		if(more() && peek() == '*') {
+	    			n.addFinalState(currState);
+	    			n.addTransition(currState, currSymb, currState);
+	    		}
+	    		n.addStartState(currState);
+	    		return n;
 		}
 	}
 	
